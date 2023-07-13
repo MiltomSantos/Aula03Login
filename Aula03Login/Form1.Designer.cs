f92b90d6c027b158991b52e0be968d5272bdbfcd
MiltomSantos <122657746+MiltomSantos@users.noreply.github.com>
2023-07-13 14:09:34 -0300

    Add files via upload

---
+++ Aula03Login/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace Aula03Login
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ldlLogin = new System.Windows.Forms.Label();
+            this.ldlSenha = new System.Windows.Forms.Label();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.btnConcluir = new System.Windows.Forms.Button();
+            this.panelSaldo = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelSaldo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ldlLogin
+            // 
+            this.ldlLogin.AutoSize = true;
+            this.ldlLogin.Location = new System.Drawing.Point(9, 19);
+            this.ldlLogin.Name = "ldlLogin";
+            this.ldlLogin.Size = new System.Drawing.Size(33, 13);
+            this.ldlLogin.TabIndex = 0;
+            this.ldlLogin.Text = "Login";
+            // 
+            // ldlSenha
+            // 
+            this.ldlSenha.AutoSize = true;
+            this.ldlSenha.Location = new System.Drawing.Point(9, 67);
+            this.ldlSenha.Name = "ldlSenha";
+            this.ldlSenha.Size = new System.Drawing.Size(38, 13);
+            this.ldlSenha.TabIndex = 1;
+            this.ldlSenha.Text = "Senha";
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(12, 35);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(162, 20);
+            this.txtLogin.TabIndex = 2;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(12, 83);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.PasswordChar = '•';
+            this.txtSenha.Size = new System.Drawing.Size(162, 20);
+            this.txtSenha.TabIndex = 3;
+            // 
+            // btnConcluir
+            // 
+            this.btnConcluir.Location = new System.Drawing.Point(12, 127);
+            this.btnConcluir.Name = "btnConcluir";
+            this.btnConcluir.Size = new System.Drawing.Size(162, 32);
+            this.btnConcluir.TabIndex = 4;
+            this.btnConcluir.Text = "Concluir";
+            this.btnConcluir.UseVisualStyleBackColor = true;
+            this.btnConcluir.Click += new System.EventHandler(this.btnConcluir_Click);
+            // 
+            // panelSaldo
+            // 
+            this.panelSaldo.Controls.Add(this.label1);
+            this.panelSaldo.Controls.Add(this.checkBox1);
+            this.panelSaldo.Controls.Add(this.textBox1);
+            this.panelSaldo.Controls.Add(this.button1);
+            this.panelSaldo.Location = new System.Drawing.Point(217, 19);
+            this.panelSaldo.Name = "panelSaldo";
+            this.panelSaldo.Size = new System.Drawing.Size(313, 150);
+            this.panelSaldo.TabIndex = 5;
+            this.panelSaldo.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Seu saldo é: RS 0";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(17, 81);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(71, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Aumentar";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(213, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(235, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Salvar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(547, 201);
+            this.Controls.Add(this.panelSaldo);
+            this.Controls.Add(this.btnConcluir);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.ldlSenha);
+            this.Controls.Add(this.ldlLogin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelSaldo.ResumeLayout(false);
+            this.panelSaldo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ldlLogin;
+        private System.Windows.Forms.Label ldlSenha;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button btnConcluir;
+        private System.Windows.Forms.Panel panelSaldo;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Aula03Login/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula03Login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConcluir_Click(object sender, EventArgs e)
+        {
+            string login = txtLogin.Text;
+            string senha = txtSenha.Text;
+
+
+            if(login=="Miltom" && senha == "123")
+            {
+                MessageBox.Show("Você está conectado!!");
+                panelSaldo.Visible = true;
+            }
+            else
+            {
+                MessageBox.Show("Tá errado kkkkkkkk");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
